a2d2176f0e95751839f6403856221c5dfd312b8d
xray-bit <uuidxx@163.com>
2024-11-15 13:04:29 +0800

    update copyright

--- sulid.go
+++ sulid.go
@@ -1,4 +1,5 @@
 // Copyright 2016 The Oklog Authors
+// Copyright 2024 @xray-bit
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
